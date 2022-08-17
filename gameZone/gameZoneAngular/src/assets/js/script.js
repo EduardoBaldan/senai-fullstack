a9da9b38ec331dd98faea2645d82b6f31b10b3ff
Eduardo Baldan <eduardo.baldan@gmail.com>
2022-08-17 08:49:59 -0300

    Atividade UC9 SA3 com framework Angular e sua biblioteca

---
+++ gameZone/gameZoneAngular/src/assets/js/script.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+    
+    let slideAtual = 1;
+    let listaSlides = ["banner-ragnarok", "banner-horizon", "banner-eldenring", "banner-spider", "banner-sekiro","banner-tsushima"]
+
+    setInterval(mudarSlide, 2000)
+
+    function mudarSlide() {
+        console.log("Slide atual:", slideAtual);
+
+        if (slideAtual > 0) {
+            $("#carrossel").removeClass(listaSlides[slideAtual - 1])
+        } else if (slideAtual == 0) {
+            $("#carrossel").removeClass(listaSlides[ (listaSlides.length) - 1 ])
+        }
+
+        $("#carrossel").addClass(listaSlides[slideAtual]);
+
+        slideAtual++
+
+        if(slideAtual > (listaSlides.length) - 1) {
+            slideAtual = 0;
+        }
+
+    }
+    
+    $("#barras").click(function() {
+
+        $("#menu").toggleClass("menu-ativo");
+        
+    })
+
+})
+
+function mostrarMenu() {
+    let menu = document.getElementById("menu")
+
+    if (getComputedStyle(menu).display == 'none') {
+        menu.style.display = 'flex';
+    } else {
+        menu.style.display = 'none';
+    }
+}
+function cadastro() {
+    let email = document.getElementById("campo-email").value
+
+    alert('Cadastrado realizado com sucesso')
+    
+}
+
+function login() {
+    let email = document.getElementById("campo-email").value
+
+    alert('Login realizado com sucesso')
+    
+}
+
+function cadastrarNewsletter() {
+    let email = document.getElementById("campo-email").value
+
+    alert('Email cadastrado com sucesso')
+    
+}
