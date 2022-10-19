07c28136bf277a14a27883f30ca13342676433fa
Eduardo Baldan <eduardo.baldan@gmail.com>
2022-10-19 20:52:27 -0300

    Codificação Back-End Encontro Remoto 2

---
+++ UC12BackEnd/BE7_FS4_UC9/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BE7_FS4_UC9.Classes;
+
+PessoaFisica novaPf = new PessoaFisica();
+
+novaPf.nome = "Lozano";
+
+Console.WriteLine(novaPf.nome);
+Console.WriteLine("Nome: " + novaPf.nome);
+Console.WriteLine($"Nome: {novaPf.nome}");
+
