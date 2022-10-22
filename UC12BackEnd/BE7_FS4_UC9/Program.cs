52a45d416db58e38b8265030b45d6d519398670f
Eduardo Baldan <eduardo.baldan@gmail.com>
2022-10-22 11:51:34 -0300

    Encontro Remoto 5

--- UC12BackEnd/BE7_FS4_UC9/Program.cs
+++ UC12BackEnd/BE7_FS4_UC9/Program.cs
@@ -1,61 +1,120 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
+using BE7_FS4_UC9;
 using BE7_FS4_UC9.Classes;
 
-/*
-PessoaFisica novaPf = new PessoaFisica();
-Endereco novoEnd = new Endereco();
-
-PessoaFisica metodoPf = new PessoaFisica();
-
-novaPf.nome = "Luiz";
-novaPf.dataNascimento = "18/02/1984";
-novaPf.cpf = "12345678900";
-novaPf.rendimento = 600.0f;
-
-novoEnd.logradouro = "Alameda Barao de Limeira";
-novoEnd.numero = 539;
-novoEnd.complemento = "Senai Informatica";
-novoEnd.endComercial = true;
-
-novaPf.endereco = novoEnd;
 
 Console.WriteLine(@$"
-Nome: {novaPf.nome}
-Endereco: {novaPf.endereco.logradouro}, {novaPf.endereco.numero}
-Maior de idade: {metodoPf.ValidarDataNascimento(novaPf.dataNascimento)}
+=================================================================================
+*                                                                               *
+*                              Seja Bem Vindo !                                 *
+*               Sistema de Cadastro de Pessoas Físicas e Jurídicas              *
+*                                                                               *
+=================================================================================
 ");
-*/
-/*
-novaPf.nome = "Lozano";
-Console.WriteLine(novaPf.nome);
-Console.WriteLine("Nome: " + novaPf.nome);
-Console.WriteLine($"Nome: {novaPf.nome}");
-*/
-
-//Console.WriteLine(novaPf.ValidarDataNascimento(new DateTime(2000,01,01)));
-
-//*Console.WriteLine(novaPf.ValidarDataNascimento(new DateTime(2000,01,01)));
-
-PessoaJuridica metodoPj = new PessoaJuridica();
-PessoaJuridica novaPj = new PessoaJuridica();
-Endereco novoEndPj = new Endereco();
-
-novaPj.nome ="NomePj";
-novaPj.cnpj ="00000000000100";
-novaPj.razaoSocial ="Razao Social Pj";
-novaPj.rendimento = 8000.5f;
-
-novoEndPj.logradouro = "Alameda Barao de Limeira";
-novoEndPj.numero = 539;
-novoEndPj.complemento = "Senai Informática";
-novoEndPj.endComercial = true;
-novaPj.endereco = novoEndPj;
-
-Console.WriteLine(@$"
-Nome: {novaPj.nome}
-Razao Social: {novaPj.razaoSocial}
-CNPJ: {novaPj.cnpj}
-CNPJ é válido: {metodoPj.ValidarCnpj(novaPj.cnpj)}");
 
 
+BarraCarregamento("Carregando",200);
+
+string? opcao;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine(@$"
+=================================================================================
+*                                                                               *
+*                            Escolha uma das Opções:                            *
+___________________________
+*                                                                               *
+*                              1 - Pessoa Física                                *
+*                              2 - Pessoa Jurídica                              *
+*                                                                               *
+*                              0  - Sair                                        *
+*                                                                               *
+=================================================================================
+");
+
+    opcao = Console.ReadLine();
+
+    switch (opcao)
+    {
+        case "1":
+            PessoaFisica novaPf = new PessoaFisica();
+            Endereco novoEnd = new Endereco();
+            PessoaFisica metodoPf = new PessoaFisica();
+            novaPf.nome = "Gerald De Rivia";
+            novaPf.dataNascimento = "01/01/1000";
+            novaPf.cpf = "001.001.111.11";
+            novaPf.rendimento = 1000.0f;
+            novoEnd.logradouro = "Carniceiro de Blaviken";
+            novoEnd.numero = 11;
+            novoEnd.complemento = "Castelo de Kaermor";
+            novoEnd.endComercial = true;
+            novaPf.endereco = novoEnd;
+
+            Console.WriteLine(@$"
+            Nome: {novaPf.nome}
+            Endereço: {novaPf.endereco.logradouro}, {novaPf.endereco.numero}
+            Maior de idade: {metodoPf.ValidarDataNascimento(novaPf.dataNascimento)}");
+            Console.WriteLine($"Digite Enter para sair");
+            Console.ReadLine();
+
+            break;
+
+
+        case "2":
+            PessoaJuridica metodoPj = new PessoaJuridica();
+            PessoaJuridica novaPj = new PessoaJuridica();
+            Endereco novoEndpj = new Endereco();
+
+            novaPj.nome = "SENAI";
+            novaPj.cnpj = "10.123.456/0001.12";
+            novaPj.razaoSocial = "Senai Informatica";
+            novaPj.rendimento = 6000;
+
+            novoEndpj.logradouro = "Avenida nao lembro";
+            novoEndpj.numero = 537;
+            novoEndpj.complemento = "casa 01";
+            novoEndpj.endComercial = true;
+
+
+            Console.WriteLine($@"
+            Nome: {novaPj.nome}
+            Razão Social: {novaPj.razaoSocial}
+            CNPJ: {novaPj.cnpj}
+            CNPJ é valido: {metodoPj.ValidarCnpj(novaPj.cnpj)}");
+
+            Console.WriteLine($"Digite Enter para sair");
+            Console.ReadLine();
+            break;
+
+        case "0":
+            Console.WriteLine($"Obrigada por utilizar nosso Sistema");
+            BarraCarregamento("Finalizando", 300);
+
+            break;
+
+        default:
+            Console.Clear();
+            Console.WriteLine($"Opção Inválida - Digite outra Opção");
+            Thread.Sleep(3000);
+            break;
+    }
+
+} while (opcao != "0");
+
+static void BarraCarregamento(string texto, int tempo)
+{
+    Console.BackgroundColor = ConsoleColor.Yellow;
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+    Console.Write($"{texto}");
+
+    for (var contador = 0; contador < 35; contador++)
+    {
+        Console.Write(". ");
+        Thread.Sleep(250);
+    }
+
+    Console.ResetColor();
+}
