993476f9b0a804c1d44f3da9defd07dc4706cdae
Eduardo Baldan <eduardo.baldan@gmail.com>
2022-10-31 14:11:12 -0300

    ER1 UC14

---
+++ UC14/ER01/Chapter/Chapter/Contexts/ChapterContext.cs
@@ -0,0 +1,28 @@
+﻿using Chapter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chapter.Contexts
+{
+    public class ChapterContext: DbContext
+    {
+        public ChapterContext()
+        {
+        }
+        public ChapterContext(DbContextOptions<ChapterContext> options) : base(options)
+        {
+        }
+        //vamos utilizar esse metodo para configurar o banco de dados
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured) 
+            {
+                //cada probedor tem sua sintaxe para especificação
+                optionsBuilder.UseSqlServer("Data Source = NOTEEDUARDOSONY\\SQLEXPRESS; initial catalog = Chapter; intergrated Security = true");
+            }
+        }
+
+        //Dbset representa as entdades que serao utilizas nas operaçoes de leitura, criação, atualização e exclusão
+        public DbSet<Livro> Livros { get; set; }
+
+    }
+}
