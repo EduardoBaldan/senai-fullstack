62406289e7b2b62d5aa33211d1db9cd3caf90346
Eduardo Baldan <eduardo.baldan@gmail.com>
2022-11-02 10:30:35 -0300

    Atualização de livros no chapter

--- UC14/ER01/script_chapter.sql
+++ UC14/ER01/script_chapter.sql
@@ -1,30 +1,3 @@
-CREATE DATABASE Chapter;
-USE Chapter;
-CREATE TABLE Livros
-(
-	Id INT PRIMARY KEY IDENTITY,
-	Titulo VARCHAR (150) NOT NULL,
-	QuantidadePaginas INT,
-	Disponivel BIT
-);
+use Chapter
 
-INSERT INTO Livros  (Titulo, QuantidadePaginas, Disponivel)
-VALUES ('itulo A', '120', '1');
-
-INSERT INTO Livros  (Titulo, QuantidadePaginas, Disponivel)
-VALUES ('itulo B', '220', '0');
-
-UPDATE Livros
-SET Titulo = 'Titulo A'
-where Id = 1;
-
-UPDATE Livros
-SET Titulo = 'Titulo B'
-where Id = 2;
-
-
-
-SELECT * FROM Livros;
-
-
-
+SELECT *FROM livros;
