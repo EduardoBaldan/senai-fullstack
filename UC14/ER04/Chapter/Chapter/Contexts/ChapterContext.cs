2b17032bc3b3df821e29abb030d121f0ca5c8e85
Eduardo Baldan <eduardo.baldan@gmail.com>
2022-11-03 12:20:33 -0300

    Atualização ER4 não roda

---
+++ UC14/ER04/Chapter/Chapter/Contexts/ChapterContext.cs
@@ -0,0 +1,30 @@
+﻿using Chapter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chapter.Contexts
+{
+    public class ChapterContext: DbContext
+    {
+        public ChapterContext()
+        {
+        }
+        public ChapterContext(DbContextOptions<ChapterContext> options) : base(options)
+        {
+        }
+        //vamos utilizar esse metodo para configurar o banco de dados
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured) 
+            {
+                //cada probedor tem sua sintaxe para especificação
+                optionsBuilder.UseSqlServer("Data Source = NOTEEDUARDOSONY\\SQLEXPRESS; initial catalog = Chapter; integrated Security = true");
+            }
+        }
+
+        //Dbset representa as entdades que serao utilizas nas operaçoes de leitura, criação, atualização e exclusão
+        public DbSet<Livro> Livros { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+    }
+}
+
