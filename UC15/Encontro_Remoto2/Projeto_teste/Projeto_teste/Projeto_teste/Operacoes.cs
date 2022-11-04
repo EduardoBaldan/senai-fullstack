60691030c528e0d83a817721b874d85c4ee9e9c6
Eduardo Baldan <eduardo.baldan@gmail.com>
2022-11-04 11:49:06 -0300

    ER2 UC15 - parte 1

---
+++ UC15/Encontro_Remoto2/Projeto_teste/Projeto_teste/Projeto_teste/Operacoes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_teste
+{
+    public static class Operacoes // Static é para não precisar instanciar para chamar
+    {
+        public static double Somar(double pNum, double sNum)
+        {
+            return (pNum + sNum);
+        }
+
+    }
+}
---
+++ UC15/Encontro_Remoto2/Projeto_teste/Projeto_teste/TesteMSteste/UnitTest1.cs
@@ -0,0 +1,43 @@
+using Projeto_teste;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NuGet.Frameworks;
+
+namespace TesteMSteste
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SomarDoisNumeros()
+        {
+            //Arrange - preparação
+            double pNum = 1;
+            double sNum = 1;
+            double rNum = 2;
+
+            //Act - Ação
+            var resultado = Operacoes.Somar(pNum, sNum);
+
+            //Assert - Verificação
+            Assert.AreEqual(rNum,resultado);
+
+        }
+
+
+        //Arrange
+        [DataTestMethod]
+        [DataRow(1,1,2)]
+        [DataRow(2,2,4)]
+        [DataRow(2,1,3)]
+
+        public void SomarDoisNumerosLista(double pNum, double sNum, double rNum)
+        {
+            //Act - Ação
+            var resultado = Operacoes.Somar(pNum, sNum);
+
+            //Assert - Verificação
+            Assert.AreEqual(rNum, resultado);
+
+        }
+    }
+}
